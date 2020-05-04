a5b35637743f8ddf3f81cac2e97a145bc6163faa
Arnel Baloyo <Arnel_Baloyo@bnz.co.nz>
2020-05-04 22:17:39 +1200

    Update Scripts

---
+++ scripts/zos-start.js
@@ -0,0 +1,6 @@
+var m1 = '#############################################################################';
+console.log(m1);
+var m2 = '############### Hey World z/OS Automation in Action! Lets Go! ###############';
+console.log(m2);
+console.log(m1);
+//# sourceMappingURL=zos-start.js.map

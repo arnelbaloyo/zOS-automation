7af3b5e79861046d0031e72f4c29ba5bad724f3c
Arnel Baloyo <Arnel_Baloyo@bnz.co.nz>
2020-05-05 13:46:41 +1200

    Remove unnecessary files

--- scripts/helloworld.js
+++
@@ -1,6 +0,0 @@
-var m1 = '#############################################################################';
-console.log(m1);
-var m2 = '############### Hey World z/OS Automation in Action! Lets Go! ###############';
-console.log(m2);
-console.log(m1);
-//# sourceMappingURL=helloworld.js.map
